afcd3256efb6eb379aa87f47ab1741733977fef8
Bukagg <nirolgg@gmail.com>
2019-02-17 16:37:04 +0200

    JPA  implementation +test codes + debuging

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -1,20 +1,22 @@
 package tacos.web;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
+
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.WebDataBinder;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.InitBinder;
+
 import lombok.extern.slf4j.Slf4j;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
@@ -30,18 +32,20 @@ import tacos.Order;
 
 
 public class DesignTacoController {
-	 private final IngredientRepository ingredientRepo;	 
-	 private TacoRepository designRepo;
-	 
-	 
+
+	    private final IngredientRepository IngredientRepo;	 
+	    private TacoRepository designRepo;
+
 	 @Autowired	 
 	 public DesignTacoController(
 	 IngredientRepository ingredientRepo,
 	 TacoRepository designRepo) {
-	 this.ingredientRepo = ingredientRepo;
+	 this.IngredientRepo = ingredientRepo;
 	 this.designRepo = designRepo;
 	 }
-
+	 
+	 
+	 
 	 @ModelAttribute(name = "order")
 	 public Order order() {
 	 return new Order();
@@ -51,34 +55,7 @@ public class DesignTacoController {
 	 return new Taco();
 	 }
 	 
-	 
-	 @InitBinder 
-	     public void initBinder(WebDataBinder binder) {	 
-	         binder.registerCustomEditor(Ingredient.class, new IngredientEditor(ingredientRepo));
-	 
-	 }
-	 
-//	 @ModelAttribute
-//	    public void addIngredientToModel(Model model) {
-//
-//		List<Ingredient> ingredients = new ArrayList<>();
-//		 ingredientRepo.findAll().forEach(i -> ingredients.add(i));
-//
-//		Type[] types = Ingredient.Type.values();
-//
-//		for (Type type : types) {
-//			model.addAttribute(type.toString().toLowerCase(),
-//					filterByType(ingredients, type));
-//		}
-//
-//		
-//	}
-//	  @GetMapping
-//	    public String showDesignForm(Model model) {
-//	        model.addAttribute("design", new Taco());
-//	        return "design";
-//	    }
-	  
+
 	  
 	  
 	  
@@ -87,9 +64,12 @@ public class DesignTacoController {
 	  
 	  @GetMapping
 	  public String showDesignForm(Model model) {
+		  System.out.println("lol1");
 	    List<Ingredient> ingredients = new ArrayList<>();
-	    ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+	    IngredientRepo.findAll().forEach(ing -> System.out.println(ing));
 	    
+	    IngredientRepo.findAll().forEach(i -> ingredients.add(i));
+	    ingredients.forEach(ing -> System.out.println(ing));
 	    Type[] types = Ingredient.Type.values();
 	    for (Type type : types) {
 	      model.addAttribute(type.toString().toLowerCase(), 
@@ -108,7 +88,7 @@ public class DesignTacoController {
 		if (errors.hasErrors()) {
 			 return "design";
 			 }
-		 
+		
 		 Taco saved = designRepo.save(design);
 		 System.out.println("test -- processDesignn -- after taco desgin save ");
 		 order.addDesign(saved);
@@ -119,15 +99,13 @@ public class DesignTacoController {
 	 return "redirect:/orders/current";
 	}
 	
-	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 	
-
-		
+	
+	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {		
 		  return ingredients
 	                .stream()
 	                .filter(x -> x.getType().equals(type))
-	                .collect(Collectors.toList());
-		
+	                .collect(Collectors.toList());		
 		
 	}
 
--- taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -12,14 +12,30 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import tacos.web.HomeController;
+import tacos.data.IngredientRepository;
+import tacos.data.OrderRepository;
+import tacos.data.TacoRepository;
+import tacos.web.WebConfig;;
+
 @RunWith(SpringRunner.class)
+@WebMvcTest
 public class HomeControllerTest {
  @Autowired
  private MockMvc mockMvc;
+ 
+ @MockBean
+ private IngredientRepository ingredientRepository;
+
+ @MockBean
+ private TacoRepository designRepository;
+
+ @MockBean
+ private OrderRepository orderRepository;
+ 
  @Test
  public void testHomePage() throws Exception {
  mockMvc.perform(get("/"))
