f67295b1a0a86843b90e21a857465f1016d3f7b9
Bukagg <nirolgg@gmail.com>
2019-02-21 14:27:59 +0200

    adding all staged changes

---
+++ taco-cloud/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,28 @@
+package tacos.data;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import tacos.Order;
+
+
+
+
+public interface OrderRepository
+extends CrudRepository<Order, Long> {
+	
+	List<Order> findByDeliveryZip(String deliveryZip);
+	
+	
+	List<Order> readOrdersByDeliveryZipAndCreatedAtBetween(
+			 String deliveryZip, Date startDate, Date endDate);
+	
+	// the old fashion way for building queries:
+	@Query("SELECT o FROM Order o WHERE o.deliveryCity='Seattle'")
+	List<Order> readOrdersDeliveredInSeattle();
+
+
+	
+}
